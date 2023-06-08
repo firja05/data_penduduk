d628bccf88c3c00ab20d8b39cfe49dbbdfa3c11d
firja05 <firjadolot@gmail.com>
2023-06-08 14:55:10 +0800

    Create update_pengguna.php

---
+++ update_pengguna.php
@@ -0,0 +1,19 @@
+<?php
+$id        = $_POST['id'];
+$nama       = $_POST['nama'];
+$usernama   = $_POST['usernama'];
+$passwordt   = md5($_POST['password']);
+
+include ('koneksi.php');
+$query = "UPDATE pengguna SET id='$id', nama='$nama',usernama='$usernama', password='$passwordt'
+where id='$id'";
+
+$update = mysqli_query($connect,$query);
+if ($update){
+    $status = "berhasil";
+
+}else{
+    $status = "gagal";
+}
+header("location:list_pengguna.php?status=".$status);   
+?>
