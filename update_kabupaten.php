e1c6ae6aeced6376dd48a8f4bc7187bb7db847fd
firja05 <firjadolot@gmail.com>
2023-06-08 14:55:02 +0800

    Create update_kabupaten.php

---
+++ update_kabupaten.php
@@ -0,0 +1,18 @@
+<?php
+$id_kabupaten        = $_POST['id_kabupaten'];
+$nama_kabupaten       = $_POST['nama_kabupaten'];
+
+
+include ('koneksi.php');
+$query = "UPDATE kab SET id_kabupaten='$id_kabupaten', nama_kabupaten='$nama_kabupaten'
+where id_kabupaten='$id_kabupaten'";
+
+$update = mysqli_query($connect,$query);
+if ($update){
+    $status = "berhasil";
+
+}else{
+    $status = "gagal";
+}
+header("location:kabupaten.php?status=".$status);   
+?>
