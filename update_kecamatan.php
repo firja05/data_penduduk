67a2be3506dd2ae7dd247995b51a232d9f3d290f
firja05 <firjadolot@gmail.com>
2023-06-08 14:55:05 +0800

    Create update_kecamatan.php

---
+++ update_kecamatan.php
@@ -0,0 +1,18 @@
+<?php
+$id_kecamatan        = $_POST['id_kecamatan'];
+$nama_kecamatan       = $_POST['nama_kecamatan'];
+
+
+include ('koneksi.php');
+$query = "UPDATE kec SET id_kecamatan='$id_kecamatan', nama_kecamatan='$nama_kecamatan'
+where id_kecamatan='$id_kecamatan'";
+
+$update = mysqli_query($connect,$query);
+if ($update){
+    $status = "berhasil";
+
+}else{
+    $status = "gagal";
+}
+header("location:kecamatan.php?status=".$status);   
+?>
