1f43d291447dd5daf1975240e186ef80d5e0eb8e
firja05 <firjadolot@gmail.com>
2023-06-11 23:01:38 +0800

    Update tambah_penduduk.php

--- tambah_penduduk.php
+++ tambah_penduduk.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'?>
 
 <div class="container mt-5">
-  <h3>Tanbah Data Penduduk</h3>
+  <h3>Tambah Data Penduduk</h3>
   <form action="simpan_penduduk.php" method="post">
     <table class="table table-striped table-bordered table-secondary" style="font-family: Arial, sans-serif;">
       <tr>
@@ -19,11 +19,12 @@
       <tr>
         <td>Kabupaten</td>
         <td>
-          <select name="id_kabupaten" class="form-select">
+          <select name="id_kabupaten" class="form-select" onchange="getKecamatan(this.value)">
+            <option value="">Pilih Kabupaten</option>
             <?php
-            $kab = mysqli_query($connect,"select * from kab");
-            while ($kabu = mysqli_fetch_array($kab)) {
-              echo "<option value='".$kabu['id_kabupaten']."'>".$kabu['nama_kabupaten']."</option>";
+            $kab = mysqli_query($connect, "SELECT * FROM kab");
+            while ($kabu = mysqli_fetch_assoc($kab)) {
+              echo "<option value='" . $kabu['id_kabupaten'] . "'>" . $kabu['nama_kabupaten'] . "</option>";
             }
             ?>
           </select>
@@ -32,13 +33,8 @@
       <tr>
         <td>Kecamatan</td>
         <td>
-          <select name="id_kecamatan" class="form-select">
-            <?php
-            $kec = mysqli_query($connect,"select * from kec");
-            while ($keca = mysqli_fetch_array($kec)) {
-              echo "<option value='".$keca['id_kecamatan']."'>".$keca['nama_kecamatan']."</option>";
-            }
-            ?>
+          <select name="id_kecamatan" id="id_kecamatan" class="form-select">
+            <option value="">Pilih Kabupaten terlebih dahulu</option>
           </select>
         </td>
       </tr>
@@ -51,9 +47,9 @@
         <td>
           <select name="id_pekerjaan" class="form-select">
             <?php
-            $pek = mysqli_query($connect,"select * from pekerjaan");
-            while ($peke = mysqli_fetch_array($pek)) {
-              echo "<option value='".$peke['id_pekerjaan']."'>".$peke['nama_pekerjaan']."</option>";
+            $pek = mysqli_query($connect, "SELECT * FROM pekerjaan");
+            while ($peke = mysqli_fetch_assoc($pek)) {
+              echo "<option value='" . $peke['id_pekerjaan'] . "'>" . $peke['nama_pekerjaan'] . "</option>";
             }
             ?>
           </select>
@@ -67,5 +63,18 @@
     </div>
   </form>
 </div>
+
+<script>
+  function getKecamatan(id_kabupaten) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("id_kecamatan").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "get_kecamatan.php?id_kabupaten=" + id_kabupaten, true);
+    xhttp.send();
+  }
+</script>
 
 <?php include 'footer.php'?>
