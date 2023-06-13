b1089667d3a81c4054fb0f877a17f208595e176d
firja05 <firjadolot@gmail.com>
2023-06-13 19:23:45 +0800

    Update update_penduduk.php

--- update_penduduk.php
+++ update_penduduk.php
@@ -1,22 +1,31 @@
 <?php
-$nik        = $_POST['nik'];
-$nama       = $_POST['nama'];
-$alamat      = $_POST['alamat'];
-$id_kabupaten   = $_POST['id_kabupaten'];
-$id_kecamatan   = $_POST['id_kecamatan'];
-$golongan_darah   = $_POST['golongan_darah'];
-$id_pekerjaan   = $_POST['id_pekerjaan'];
+$nik = $_POST['nik'];
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$id_kabupaten = $_POST['id_kabupaten'];
+$id_kecamatan = $_POST['id_kecamatan'];
+$golongan_darah = $_POST['golongan_darah'];
+$id_pekerjaan = $_POST['id_pekerjaan'];
 
-include ('koneksi.php');
-$query = "UPDATE penduduk SET nik='$nik', nama='$nama',alamat='$alamat', id_kabupaten='$id_kabupaten',id_kecamatan='$id_kecamatan', golongan_darah='$golongan_darah', id_pekerjaan='$id_pekerjaan' 
-where nik='$nik'";
+include('koneksi.php');
 
-$update = mysqli_query($connect,$query);
-if ($update){
+$query = "UPDATE penduduk SET nama=?, alamat=?, id_kabupaten=?, id_kecamatan=?, golongan_darah=?, id_pekerjaan=? WHERE nik=?";
+$stmt = mysqli_prepare($connect, $query);
+mysqli_stmt_bind_param($stmt, "ssiissi", $nama, $alamat, $id_kabupaten, $id_kecamatan, $golongan_darah, $id_pekerjaan, $nik);
+$update = mysqli_stmt_execute($stmt);
+
+if ($update) {
     $status = "berhasil";
-
-}else{
+} else {
     $status = "gagal";
+    // Tampilkan pesan kesalahan spesifik
+    echo "Error: " . mysqli_error($connect);
 }
-header("location:index.php?status=".$status);   
+
+mysqli_stmt_close($stmt);
+mysqli_close($connect);
+
+header("location: edit_penduduk.php?status=".$status."&nik=".$nik);
+exit();
+
 ?>
