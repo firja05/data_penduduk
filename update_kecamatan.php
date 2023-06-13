178ad7e551c1b2d1417c05a7fa877c8d815b7eef
firja05 <firjadolot@gmail.com>
2023-06-13 19:23:51 +0800

    Update update_kecamatan.php

--- update_kecamatan.php
+++ update_kecamatan.php
@@ -1,10 +1,11 @@
 <?php
 $id_kecamatan        = $_POST['id_kecamatan'];
+$id_kabupaten       = $_POST['id_kabupaten'];
 $nama_kecamatan       = $_POST['nama_kecamatan'];
 
 
 include ('koneksi.php');
-$query = "UPDATE kec SET id_kecamatan='$id_kecamatan', nama_kecamatan='$nama_kecamatan'
+$query = "UPDATE kec SET id_kecamatan='$id_kecamatan', id_kabupaten='$id_kabupaten', nama_kecamatan='$nama_kecamatan'
 where id_kecamatan='$id_kecamatan'";
 
 $update = mysqli_query($connect,$query);
