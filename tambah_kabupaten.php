9c2e0dbbfeb8187f9f3fbc104104cbc91026c45c
firja05 <firjadolot@gmail.com>
2023-06-13 19:23:56 +0800

    Update tambah_kabupaten.php

--- tambah_kabupaten.php
+++ tambah_kabupaten.php
@@ -2,6 +2,17 @@
 
 <div class="container mt-5">
 <h3>Tambah Kabupaten</h3>
+<?php if (isset($_GET['status'])) : ?>
+    <?php if ($_GET['status'] === 'berhasil') : ?>
+      <div id="success-alert" class="alert alert-success" role="alert">
+        Berhasil tambahkan data!
+      </div>
+    <?php elseif ($_GET['status'] === 'gagal') : ?>
+      <div id="error-alert" class="alert alert-danger" role="alert">
+        Gagal tambahkan data!
+      </div>
+      <?php endif; ?>
+  <?php endif; ?>
 <form action="simpan_kabupaten.php" method="post">
     <table class="table table-striped table-bordered table-secondary" style="font-family: Arial, sans-serif;">
             <tr>
@@ -19,4 +30,21 @@
     </div>
   </form>
 </div>
+<script>
+  // Hide success alert after 2 seconds
+  setTimeout(function() {
+    var successAlert = document.getElementById('success-alert');
+    if (successAlert) {
+      successAlert.style.display = 'none';
+    }
+  }, 2000);
+
+  // Hide error alert after 2 seconds
+  setTimeout(function() {
+    var errorAlert = document.getElementById('error-alert');
+    if (errorAlert) {
+      errorAlert.style.display = 'none';
+    }
+  }, 2000);
+  </script>
 <?php include 'footer.php'?>
