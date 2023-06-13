082891cbaeebc2fe33086f5cc6d5434dd3136c61
firja05 <firjadolot@gmail.com>
2023-06-13 21:06:05 +0800

    Update kecamatan.php

--- kecamatan.php
+++ kecamatan.php
@@ -1,4 +1,27 @@
+<style>
+  .alert {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin-top: 10px;
+  }
+</style>
+
 <?php include 'header.php'; ?>
+
+<?php if (isset($_GET['status'])) : ?>
+      <?php if ($_GET['status'] === 'berhasil') : ?>
+        <div id="success-alert" class="alert alert-danger" role="alert">
+        <i class="bi bi-trash-fill"></i> Data Berhasil Dihapus!
+        </div>
+      <?php elseif ($_GET['status'] === 'gagal') : ?>
+        <div id="error-alert" class="alert alert-danger" role="alert">
+          Data Gagal Dihapus!
+        </div>
+      <?php endif; ?>
+    <?php endif; ?>
+      
 
 <div class="container my-5">
     <h1 class="text-center mb-4">Data Kecamatan</h1>
@@ -63,5 +86,22 @@
         </table>
     </div>
 </div>
+<script>
+  // Hide success alert after 2 seconds
+  setTimeout(function() {
+    var successAlert = document.getElementById('success-alert');
+    if (successAlert) {
+      successAlert.style.display = 'none';
+    }
+  }, 2000);
+
+  // Hide error alert after 2 seconds
+  setTimeout(function() {
+    var errorAlert = document.getElementById('error-alert');
+    if (errorAlert) {
+      errorAlert.style.display = 'none';
+    }
+  }, 2000);
+</script>
 
 <?php include 'footer.php'; ?>
