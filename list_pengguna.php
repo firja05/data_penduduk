a2e5476a1f19dc97ba9befa83ec75c31853c9d3a
firja05 <firjadolot@gmail.com>
2023-06-13 21:06:10 +0800

    Update list_pengguna.php

--- list_pengguna.php
+++ list_pengguna.php
@@ -1,4 +1,27 @@
-<?php include 'header.php'?>
+<style>
+  .alert {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin-top: 10px;
+    z-index: 9999;
+  }
+</style>
+
+<?php include 'header.php' ?> 
+
+<?php if (isset($_GET['status'])) : ?>
+      <?php if ($_GET['status'] === 'berhasil') : ?>
+        <div id="success-alert" class="alert alert-danger" role="alert">
+        <i class="bi bi-trash-fill"></i> Data Berhasil Dihapus!
+        </div>
+      <?php elseif ($_GET['status'] === 'gagal') : ?>
+        <div id="error-alert" class="alert alert-danger" role="alert">
+          Data Gagal Dihapus!
+        </div>
+      <?php endif; ?>
+    <?php endif; ?>
 
 <div class="container my-5">
 <h1 class="text-center mb-4">Data Pengguna</h1>
@@ -41,7 +64,7 @@
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Hapus Data Kabupaten</h5>
+                                        <h5 class="modal-title" id="exampleModalLabel">Hapus Data Pengguna</h5>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
@@ -63,5 +86,21 @@
     </tbody>
   </table>
 </div>
+<script>
+  // Hide success alert after 2 seconds
+  setTimeout(function() {
+    var successAlert = document.getElementById('success-alert');
+    if (successAlert) {
+      successAlert.style.display = 'none';
+    }
+  }, 2000);
 
+  // Hide error alert after 2 seconds
+  setTimeout(function() {
+    var errorAlert = document.getElementById('error-alert');
+    if (errorAlert) {
+      errorAlert.style.display = 'none';
+    }
+  }, 2000);
+</script>
 <?php include 'footer.php'?>
