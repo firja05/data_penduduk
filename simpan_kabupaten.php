020a7a0ce5c20c3c70549aefde62efdb29742420
firja05 <firjadolot@gmail.com>
2023-06-13 19:24:04 +0800

    Update simpan_kabupaten.php

--- simpan_kabupaten.php
+++ simpan_kabupaten.php
@@ -4,6 +4,16 @@ $nama_kabupaten      = $_POST['nama_kabupaten'];
  
 
 include ('koneksi.php');
+
+$check_query = "SELECT nama_kabupaten FROM kab WHERE nama_kabupaten = '$nama_kabupaten'";
+$check_result = mysqli_query($connect, $check_query);
+
+if (mysqli_num_rows($check_result) > 0) {
+    $status = "gagal";
+    header("location:tambah_kabupaten.php?status=" . $status);
+    exit();
+}
+
 $query = "INSERT INTO kab(id_kabupaten, nama_kabupaten)
 VALUES('$id_kabupaten', '$nama_kabupaten')";
 $insert = mysqli_query($connect,$query);
@@ -14,5 +24,5 @@ if ($insert){
 }else{
     $status = "gagal";
 }
-header("location:index.php?status=".$status);   
+header("location:tambah_kabupaten.php?status=".$status);   
 ?>
