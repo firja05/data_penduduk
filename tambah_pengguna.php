6caf5512bc4037adec35faf6398812de973ccaab
firja05 <firjadolot@gmail.com>
2023-06-13 21:06:28 +0800

    Update tambah_pengguna.php

--- tambah_pengguna.php
+++ tambah_pengguna.php
@@ -1,4 +1,36 @@
+<style>
+  .alert {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin-top: 10px;
+    z-index: 9999;
+  }
+</style>
+
 <?php include 'header.php'?>
+
+<?php if (isset($_GET['status'])) : ?>
+  <?php if ($_GET['status'] === 'berhasil') : ?>
+    <div id="success-alert" class="alert alert-success" role="alert">
+      Berhasil tambahkan data!
+    </div>
+  <?php elseif ($_GET['status'] === 'gagal_id') : ?>
+    <div id="error-alert" class="alert alert-danger" role="alert">
+      Gagal tambahkan data! ID sudah ada.
+    </div>
+  <?php elseif ($_GET['status'] === 'gagal_usernama') : ?>
+    <div id="error-alert" class="alert alert-danger" role="alert">
+      Gagal tambahkan data! Username sudah ada.
+    </div>
+  <?php elseif ($_GET['status'] === 'gagal') : ?>
+    <div id="error-alert" class="alert alert-danger" role="alert">
+      Gagal tambahkan data!
+    </div>
+  <?php endif; ?>
+<?php endif; ?>
+
 
 <div class="container mt-5">
 <h3>Tambah Data Pengguna</h3>
@@ -27,4 +59,21 @@
     </div>
   </form>
 </div>
+<script>
+  // Hide success alert after 2 seconds
+  setTimeout(function() {
+    var successAlert = document.getElementById('success-alert');
+    if (successAlert) {
+      successAlert.style.display = 'none';
+    }
+  }, 2000);
+
+  // Hide error alert after 2 seconds
+  setTimeout(function() {
+    var errorAlert = document.getElementById('error-alert');
+    if (errorAlert) {
+      errorAlert.style.display = 'none';
+    }
+  }, 2000);
+</script>
 <?php include 'footer.php'?>
