3167af536d73e5f551679d8c53d03696de03c4e6
firja05 <firjadolot@gmail.com>
2023-06-13 19:23:59 +0800

    Update simpan_penduduk.php

--- simpan_penduduk.php
+++ simpan_penduduk.php
@@ -1,23 +1,33 @@
 <?php
-$nik        = $_POST['nik'];
-$nama       = $_POST['nama'];
-$alamat      = $_POST['alamat'];
-$id_kabupaten   = $_POST['id_kabupaten'];
-$id_kecamatan   = $_POST['id_kecamatan'];
-$golongan_darah   = $_POST['golongan_darah'];
-$id_pekerjaan   = $_POST['id_pekerjaan'];
+$nik = $_POST['nik'];
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$id_kabupaten = $_POST['id_kabupaten'];
+$id_kecamatan = $_POST['id_kecamatan'];
+$golongan_darah = $_POST['golongan_darah'];
+$id_pekerjaan = $_POST['id_pekerjaan'];
 
+include('koneksi.php');
 
-include ('koneksi.php');
+$check_query = "SELECT nik FROM penduduk WHERE nik = '$nik'";
+$check_result = mysqli_query($connect, $check_query);
+
+if (mysqli_num_rows($check_result) > 0) {
+    $status = "gagal";
+    header("location:tambah_penduduk.php?status=" . $status);
+    exit();
+}
+
 $query = "INSERT INTO penduduk(nik, nama, alamat, id_kabupaten, id_kecamatan, golongan_darah, id_pekerjaan)
 VALUES('$nik', '$nama', '$alamat', '$id_kabupaten', '$id_kecamatan', '$golongan_darah', '$id_pekerjaan')";
-$insert = mysqli_query($connect,$query);    
 
-if ($insert){
+$insert = mysqli_query($connect, $query);
+
+if ($insert) {
     $status = "berhasil";
-
-}else{
+} else {
     $status = "gagal";
 }
-header("location:index.php?status=".$status);   
+
+header("location:tambah_penduduk.php?status=" . $status);
 ?>
