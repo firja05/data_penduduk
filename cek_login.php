704c090989e28196e3033afcbe7fb325f39720b3
firja05 <firjadolot@gmail.com>
2023-06-14 19:58:14 +0800

    Update cek_login.php

--- cek_login.php
+++ cek_login.php
@@ -1,20 +1,20 @@
 <?php
-$usernameTxt    = $_POST['usernama'];
-$passwordTxt    = md5($_POST['password']);
+$usernameTxt = $_POST['usernama'];
+$passwordTxt = md5($_POST['password']);
 
 include 'koneksi.php';
 
-$pengguna=mysqli_query($connect,"select * from pengguna where usernama='$usernameTxt' and password='$passwordTxt'");
+$pengguna = mysqli_query($connect, "SELECT * FROM pengguna WHERE usernama='$usernameTxt' AND password='$passwordTxt'");
 
-$check=mysqli_num_rows($pengguna);
+$check = mysqli_num_rows($pengguna);
 
-if ($check>0){
+if ($check > 0) {
     session_start();
     $penggunaRow = mysqli_fetch_array($pengguna);
     $_SESSION['usernama'] = $penggunaRow['usernama'];
-    $_SESSION['nama']   = $penggunaRow['nama'];
-    header("location:index.php");
-}else{
-    header("location:login.php");
+    $_SESSION['nama'] = $penggunaRow['nama'];
+    header("location:index.php?status=berhasil_login");
+} else {
+    header("location:login.php?status=gagal_login");
 }
 ?>
